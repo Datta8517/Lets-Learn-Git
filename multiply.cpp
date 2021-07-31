4c3f68126fcfaf7588349d23ac18a43afc42d2e8
Dattatray Rajesh Jamkhande <dattatreyjamkhande@gmail.com>
2021-07-31 12:14:44 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+    int sum;
+
+    cin>>a>>b;
+    mul=a*b;
+    cout<<"The multiplication is "<<mul<<endl;
+
+    return 0;
+}
